5a76de6fb482667e151e0f3317afd2e957967a9c
abhiiii.iiii <abhijithviju2005@gmail.com>
2024-06-21 23:30:45 +0400

    css madness

---
+++ portfolio/src/components/home/Data.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Data = () => {
+    return (
+        <div className="home__data">
+            <h1 className="home__title">Abhijith Viju Priya Rani</h1>
+        </div>
+    )
+}
+
+export default Data;
