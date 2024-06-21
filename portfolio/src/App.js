218e088118b656ec7bd609193d36d4c80f639ade
abhiiii.iiii <abhijithviju2005@gmail.com>
2024-06-21 22:15:37 +0400

    initiating the website and giving is some color

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/header/Header";
+const App = () => {
+  return (
+      <>
+        <Header />
+      </>
+  );
+}
+
+export default App;
