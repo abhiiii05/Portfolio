461b116b9cf8336444380235c05a10b2a82f0d3f
abhiiii.iiii <abhijithviju2005@gmail.com>
2024-06-23 15:36:12 +0400

    about madness

--- portfolio/src/components/about/Info.jsx
+++ portfolio/src/components/about/Info.jsx
@@ -2,7 +2,28 @@ import React from "react";
 
 const Info = () =>{
     return(
-        <div>Info</div>
+        <div className="about__info grid">
+            <div className="about__box">
+            <i className='bx bx-award about__icon'></i>
+
+                <h3 className="about__title">Experience</h3>
+                <span className="about__subtitle">3 Years</span>
+            </div>
+
+            <div className="about__box">
+            <i class='bx bx-briefcase about__icon'></i>
+
+                <h3 className="about__title">Completed</h3>
+                <span className="about__subtitle">10 + projects</span>
+            </div>
+
+            <div className="about__box">
+            <i class='bx bx-support about__icon'></i>
+
+                <h3 className="about__title">Support</h3>
+                <span className="about__subtitle">Online 24/7</span>
+            </div>
+        </div>
     )
 }
 
