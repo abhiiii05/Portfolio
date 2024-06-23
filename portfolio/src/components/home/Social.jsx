50863f43af43ac44d5f4a6d163435107f49c13f8
abhiiii.iiii <abhijithviju2005@gmail.com>
2024-06-23 22:28:08 +0400

    linking

--- portfolio/src/components/home/Social.jsx
+++ portfolio/src/components/home/Social.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+            <a href="https://www.instagram.com/abhiiii.iiii?igsh=d2hnMHJzY2NlYXI5" className="home__social-icon" target="_blank">
                 <i className="uil uil-instagram"></i>
             </a>
 
-            <a href="" className="home__social-icon" target="_blank">
-                <i className="uil uil-dribbble"></i>
+            <a href="https://www.linkedin.com/in/abhijith-viju" className="home__social-icon" target="_blank">
+                <i className="uil uil-linkedin-alt"></i>
             </a>
 
-            <a href="https://github.com/" className="home__social-icon" target="_blank">
+            <a href="https://github.com/abhiiii05" className="home__social-icon" target="_blank">
                 <i className="uil uil-github-alt"></i>
             </a>
         </div>
